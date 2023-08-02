ed279829bc29a3bd2a8fc6b246b74f05ae295cf4
portableCoder <>
2023-08-02 15:55:47 +0530

    Updates

---
+++ _astro/useSection.087fa920.js
@@ -0,0 +1,1 @@
+import{r as n}from"./index.13fb2395.js";import{d as i,b as u,s as o}from"./jsx-runtime.0a5ddef3.js";const f=t=>{const[r,s]=i({amount:.1}),e=u(o);return n.useEffect(()=>{s&&t&&o.set({...e,section:t})},[s]),[r,e,s]};export{f as u};
